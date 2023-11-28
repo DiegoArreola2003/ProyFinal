c330a71a6ea63d312533b5b1e7df72272ad87b87
DiegoArreola2003 <diegoarreola88@aragon.unam.mx>
2023-11-27 21:19:39 -0600

    Modificación de algunos datos de la base de datos

--- db.sql
+++ db.sql
@@ -2,12 +2,12 @@ CREATE DATABASE UNI;
 use UNI;
 
 CREATE TABLE usuario(
-    nombre_usuario VARCHAR(20),
-    primer_apellido VARCHAR(10),
-    segundo_apellido VARCHAR(10),
-    email VARCHAR(25) UNIQUE,
-    ciudad VARCHAR(30),
-	edad INT NOT NULL,
-    contrasena VARCHAR(12),
+    Nombre VARCHAR(20),
+    Primer_apellido VARCHAR(10),
+    Segundo_apellido VARCHAR(10),
+    Email VARCHAR(25) UNIQUE,
+    Ciudad VARCHAR(30),
+	Edad INT NOT NULL,
+    Contrasena VARCHAR(12),
     PRIMARY KEY (id_cliente)
 	);
